714e549141a7c8778c853c64a872b2cf763a471f
rohan <rohanasif@yahoo.com>
2022-10-08 07:37:33 +0500

    Add functions

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,5 +1,5 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
-
+products = [];
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
    - name: name of product (string)
@@ -8,7 +8,29 @@
    - productId: unique id for the product (number)
    - image: picture of product (url string)
 */
+const cherry = {
+  name: "Cherry",
+  price: 10,
+  quantity: 0,
+  productId: 1,
+  image: "../images/cherry.jpg"
+}
 
+const orange = {
+  name: "Orange",
+  price: 15,
+  quantity: 0,
+  productId: 2,
+  image: "../images/orange.jpg"
+}
+
+const strawberry = {
+  name: "Strawberry",
+  price: 3,
+  quantity: 0,
+  productId: 3,
+  image: "../images/strawberry.jpg"
+}
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
    - orange.jpg by Mae Mu
@@ -16,42 +38,54 @@
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
-
+cart = []
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
+function addProductToCart(productId){
 
+}
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+function increaseQuantity(productId){
 
+}
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+function decreaseQuantity(productId){
 
+}
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+function cartTotal(){
 
+}
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
 
 /* Create a function called emptyCart that empties the products from the cart */
+function emptyCart(){
 
+}
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
-
+function pay(amount){
+  
+}
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
 

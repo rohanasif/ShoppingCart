47a08134b83c1a05742b310661d1c066f1b24b0c
Rohan <rohanasif1990@gmail.com>
2022-10-10 04:19:36 +0500

    Solve all issues expect cartTotal function

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,5 +1,27 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
-const products = [];
+const products = [
+  {
+  name: "Cherry",
+  price: 10,
+  quantity: 0,
+  productId: 1,
+  image: "../images/cherry.jpg"
+},
+{
+  name: "Orange",
+  price: 15,
+  quantity: 0,
+  productId: 2,
+  image: "../images/orange.jpg"
+},
+{
+  name: "Strawberry",
+  price: 3,
+  quantity: 0,
+  productId: 3,
+  image: "../images/strawberry.jpg"
+}
+];
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
    - name: name of product (string)
@@ -8,33 +30,7 @@ const products = [];
    - productId: unique id for the product (number)
    - image: picture of product (url string)
 */
-let totalPaid = 30;
-const cherry = {
-  name: "Cherry",
-  price: 10,
-  quantity: 0,
-  productId: 1,
-  image: "../images/cherry.jpg"
-}
 
-const orange = {
-  name: "Orange",
-  price: 15,
-  quantity: 0,
-  productId: 2,
-  image: "../images/orange.jpg"
-}
-
-const strawberry = {
-  name: "Strawberry",
-  price: 3,
-  quantity: 0,
-  productId: 3,
-  image: "../images/strawberry.jpg"
-}
-products[0] = cherry;
-products[1] = orange;
-products[2] = strawberry;
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
    - orange.jpg by Mae Mu
@@ -48,46 +44,21 @@ let cart = []
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
-function addProductToCart(productId){
-  switch (productId){
-    case 1:
-      cherry.quantity += 1;
-      cart += cherry;
-      break;
-    case 2:
-      orange.quantity += 1;
-      cart += orange;
-      break;
-    case 3:
-      strawberry.quantity += 1;
-      cart += strawberry;
-      break;
-    default:
-      console.log("Invalid input")
-      break;
-  }
-  
-      
+function addProductToCart(Identity){
+  let product = products.find(product => product.productId === Identity);
+  product.quantity += 1;
 
+  if (!cart.includes(product)) {
+    cart.push(product)
+  }
 }
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
-function increaseQuantity(productId){
-  switch (productId){
-    case 1:
-      cherry.quantity += 1;
-      break;
-    case 2:
-      orange.quantity += 1;
-      break;
-    case 3:
-      strawberry.quantity += 1;
-      break;
-    default:
-      console.log("Invalid input")
-  }
+function increaseQuantity(Identity){
+  let product = products.find(product => product.productId === Identity);
+  product.quantity += 1;
   
 }
 /* Create a function named decreaseQuantity that takes in the productId as an argument
@@ -95,47 +66,18 @@ function increaseQuantity(productId){
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
-function decreaseQuantity(productId){
-  switch (productId){
-    case 1:
-      cherry.quantity -= 1;
-      if (cherry.quantity === 0) {
-        cart.removeProductFromCart(1)
-      }
-      break;
-    case 2:
-      orange.quantity -= 1;
-      if (orange.quantity === 0) {
-        cart.removeProductFromCart(2)
-      }
-      break;
-    case 3:
-      strawberry.quantity -= 1;
-      if (strawberry.quantity === 0) {
-        cart.removeProductFromCart(3)
-      }
-      break;
-    default:
-      console.log("Invalid input")
-      break;
-  }
+function decreaseQuantity(Identity){
+  let product = products.find(product => product.productId === Identity);
+  product.quantity -= 1
+  if(product.quantity == 0) {
+    cart.splice(cart.indexOf(product), 1);
+  };
 }
 
-function removeProductFromCart(productId){
-  switch (productId){
-    case 1:
-      cherry.quantity = 0;
-      break;
-    case 2:
-      orange.quantity = 0;
-      break;
-    case 3:
-      strawberry.quantity = 0;
-      break;
-    default:
-      console.log("Invalid input")
-      break;
-  }
+function removeProductFromCart(Indentity){
+  let product = products.find(product => product.productId === Indentity);
+  product.quantity = 0;
+  cart.splice(cart.indexOf(product), 1);
 }
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
@@ -145,7 +87,9 @@ function removeProductFromCart(productId){
 function cartTotal(){
   let sum = 0;
   for(let i=0;i<3;i++){
-    sum+=(cart[i].price)*(cart[i].quantity);
+    let productPrice = cart[i].price;
+    let productQuantity = cart[i].quantity;
+    sum+=(productPrice*productQuantity);
   }
   return sum;
 }
@@ -162,8 +106,10 @@ function emptyCart(){
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
+let totalPaid = 0;
 function pay(amount){
-  return totalPaid - amount;
+  totalPaid += amount;
+  return totalPaid - cartTotal()
 }
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
